3d74fb74390405d27bb11ed8525c5bad5c67ba67
qzvk <reisuo@protonmail.com>
2022-10-17 14:42:51 +0100

    Add lexer to main.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::io::{stdin, Read};
+
 mod lexer;
 
 fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    for (kind, info) in lexer::lex(&input) {
+        println!("{info} {kind}");
+    }
 }
