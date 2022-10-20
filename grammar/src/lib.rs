a74c7557c51d9b1e9f4a0a916fb2eac25207c258
qzvk <reisuo@protonmail.com>
2022-10-20 10:48:26 +0100

    Add documentation to grammar crate.

--- grammar/src/lib.rs
+++ grammar/src/lib.rs
@@ -1,20 +1,28 @@
 #![forbid(unsafe_code)]
+#![warn(missing_docs)]
 
+//! Library for validating grammars and generating parse tables.
+
+/// A context-free grammar.
 pub struct Grammar<N, T> {
     start: Symbol<N, T>,
     rules: Vec<(N, Vec<Symbol<N, T>>)>,
 }
 
 impl<N, T> Grammar<N, T> {
+    /// Create a new context free grammar with terminals `T`, nonterminals `N`, start symbol
+    /// `start` and no rules.
     pub const fn new(start: Symbol<N, T>) -> Self {
         let rules = Vec::new();
         Self { start, rules }
     }
 
+    /// The grammar's start symbol.
     pub const fn start(&self) -> &Symbol<N, T> {
         &self.start
     }
 
+    /// Add a rewrite rule to the grammar `left -> right`. Duplicate rules are ignored.
     pub fn add_rule(&mut self, left: N, right: impl Into<Vec<Symbol<N, T>>>)
     where
         N: Ord,
@@ -27,14 +35,19 @@ impl<N, T> Grammar<N, T> {
         }
     }
 
+    /// A sorted, unique list of the grammar's rewrite rules.
     pub fn rules(&self) -> impl Iterator<Item = &(N, Vec<Symbol<N, T>>)> {
         self.rules.iter()
     }
 }
 
+/// A grammar symbol. Either a nonterminal or terminal.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Symbol<N, T> {
+    /// A nonterminal symbol.
     Nonterminal(N),
+
+    /// A terminal symbol.
     Terminal(T),
 }
 
