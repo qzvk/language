2b45558b9a4d65e78b956bf7378cd231c2443bd3
qzvk <reisuo@protonmail.com>
2022-10-21 17:34:20 +0100

    Rewrite grammar creation.

--- grammar/src/lib.rs
+++ grammar/src/lib.rs
@@ -4,86 +4,199 @@
 //! Library for validating grammars and generating parse tables.
 
 /// A context-free grammar.
-pub struct Grammar<N, T> {
-    start: Symbol<N, T>,
-    rules: Vec<(N, Vec<Symbol<N, T>>)>,
+pub struct Grammar {
+    nonterminal_count: u32,
+    terminal_count: u32,
+    rules: Vec<(Nonterminal, Vec<Symbol>)>,
 }
 
-impl<N, T> Grammar<N, T> {
-    /// Create a new context free grammar with terminals `T`, nonterminals `N`, start symbol
-    /// `start` and no rules.
-    pub const fn new(start: Symbol<N, T>) -> Self {
-        let rules = Vec::new();
-        Self { start, rules }
+impl Grammar {
+    /// Create a new grammar, returning its start nonterminal and itself.
+    pub fn new() -> (Nonterminal, Self) {
+        let start = Nonterminal(0);
+
+        let grammar = Self {
+            nonterminal_count: 0,
+            terminal_count: 0,
+            rules: Vec::new(),
+        };
+
+        (start, grammar)
     }
 
-    /// The grammar's start symbol.
-    pub const fn start(&self) -> &Symbol<N, T> {
-        &self.start
+    /// Add a new, unique nonterminal to the grammar.
+    pub fn add_nonterminal(&mut self) -> Nonterminal {
+        self.nonterminal_count += 1;
+        Nonterminal(self.nonterminal_count)
     }
 
-    /// Add a rewrite rule to the grammar `left -> right`. Duplicate rules are ignored.
-    pub fn add_rule(&mut self, left: N, right: impl Into<Vec<Symbol<N, T>>>)
-    where
-        N: Ord,
-        T: Ord,
-    {
-        let rule = (left, right.into());
+    /// Add a new, unique terminal to the grammar.
+    pub fn add_terminal(&mut self) -> Terminal {
+        self.terminal_count += 1;
+        Terminal(self.terminal_count)
+    }
 
-        if let Err(index) = self.rules.binary_search(&rule) {
-            self.rules.insert(index, rule);
+    /// Add a rule to the grammar.
+    pub fn add_rule(&mut self, left: Nonterminal) -> GrammarRule {
+        let rule = (left, Vec::new());
+        self.rules.push(rule);
+
+        // Safe to unwrap, we definitely have at least 1 element.
+        let rule = self.rules.last_mut().unwrap();
+
+        GrammarRule { rule }
+    }
+
+    /// The number of rules of the grammar.
+    pub fn rule_count(&self) -> usize {
+        self.rules.len()
+    }
+
+    /// If the grammar is proper, return `Ok`, otherwise return `Err`.
+    pub fn validate(self) -> Result<ProperGrammar, Error> {
+        let mut productive = vec![false; self.nonterminal_count as usize + 1];
+
+        loop {
+            let mut changed = false;
+
+            for (left, right) in &self.rules {
+                let mut is_definitely_productive = true;
+
+                for r in right {
+                    if let Symbol::Nonterminal(n) = r {
+                        if !productive[n.0 as usize] {
+                            is_definitely_productive = false;
+                            break;
+                        }
+                    }
+                }
+
+                if is_definitely_productive && !productive[left.0 as usize] {
+                    productive[left.0 as usize] = true;
+                    changed = true;
+                }
+            }
+
+            if changed {
+                break;
+            }
+        }
+
+        let mut unproductive_nonterminals = Vec::new();
+        for (p, is_productive) in productive.into_iter().enumerate() {
+            if !is_productive {
+                unproductive_nonterminals.push(Nonterminal(p as u32));
+            }
+        }
+
+        if unproductive_nonterminals.is_empty() {
+            Ok(ProperGrammar {})
+        } else {
+            Err(Error::UnproductiveNonterminals(unproductive_nonterminals))
         }
     }
+}
 
-    /// A sorted, unique list of the grammar's rewrite rules.
-    pub fn rules(&self) -> impl Iterator<Item = (&N, &[Symbol<N, T>])> {
-        self.rules.iter().map(|(l, r)| (l, r.as_ref()))
+/// A rewrite rule of a context-free grammar.
+pub struct GrammarRule<'a> {
+    rule: &'a mut (Nonterminal, Vec<Symbol>),
+}
+
+impl<'a> GrammarRule<'a> {
+    /// Append a terminal to the of the rule.
+    pub fn terminal(self, t: Terminal) -> Self {
+        self.symbol(Symbol::Terminal(t))
+    }
+
+    /// Append a nonterminal to the of the rule.
+    pub fn nonterminal(self, n: Nonterminal) -> Self {
+        self.symbol(Symbol::Nonterminal(n))
+    }
+
+    fn symbol(self, s: Symbol) -> Self {
+        self.rule.1.push(s);
+        self
     }
 }
 
-/// A grammar symbol. Either a nonterminal or terminal.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Symbol<N, T> {
-    /// A nonterminal symbol.
-    Nonterminal(N),
+/// A nonterminal symbol of a grammar.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Nonterminal(u32);
 
-    /// A terminal symbol.
-    Terminal(T),
+/// A terminal symbol of a grammar.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Terminal(u32);
+
+/// A proper context-free grammar. See [`Grammar::validate`].
+#[derive(Debug)]
+pub struct ProperGrammar {}
+
+/// An error in a non-proper grammar.
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    /// One or more nonterminals can never produce a string of terminals during derivation.
+    UnproductiveNonterminals(Vec<Nonterminal>),
+}
+
+enum Symbol {
+    Nonterminal(Nonterminal),
+    Terminal(Terminal),
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Grammar, Symbol};
+    use super::{Error, Grammar, Nonterminal, ProperGrammar, Terminal};
 
     #[test]
     fn can_create_grammar() {
-        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-        enum Nonterminal {
-            A,
-        }
-        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-        enum Terminal {
-            B,
-            C,
-        }
+        let (start, mut grammar): (Nonterminal, Grammar) = Grammar::new();
 
-        const START: Symbol<Nonterminal, Terminal> = Symbol::Nonterminal(Nonterminal::A);
+        let a: Nonterminal = grammar.add_nonterminal();
+        assert_ne!(start, a);
 
-        let mut grammar = Grammar::new(START);
-        grammar.add_rule(Nonterminal::A, [Symbol::Terminal(Terminal::C)]); // Should be sorted after A -> B.
-        grammar.add_rule(Nonterminal::A, [Symbol::Terminal(Terminal::B)]);
-        grammar.add_rule(Nonterminal::A, [Symbol::Terminal(Terminal::B)]); // Duplicates should be discarded.
+        let b: Nonterminal = grammar.add_nonterminal();
+        assert_ne!(start, b);
+        assert_ne!(a, b);
 
-        let start = grammar.start();
-        assert_eq!(&START, start);
+        let c: Terminal = grammar.add_terminal();
+        let d: Terminal = grammar.add_terminal();
+        assert_ne!(c, d);
 
-        let rules: Vec<_> = grammar.rules().collect();
-        assert_eq!(
-            vec![
-                (&Nonterminal::A, [Symbol::Terminal(Terminal::B)].as_slice()),
-                (&Nonterminal::A, [Symbol::Terminal(Terminal::C)].as_slice()),
-            ],
-            rules
-        );
+        assert_eq!(0, grammar.rule_count());
+        grammar.add_rule(a); // Epsilon rule.
+        assert_eq!(1, grammar.rule_count());
+
+        grammar.add_rule(a).terminal(c);
+        assert_eq!(2, grammar.rule_count());
+
+        grammar.add_rule(b).terminal(d);
+        assert_eq!(3, grammar.rule_count());
+
+        grammar.add_rule(a).terminal(d).nonterminal(b).terminal(c);
+        assert_eq!(4, grammar.rule_count());
+
+        grammar.add_rule(start).nonterminal(a);
+        assert_eq!(5, grammar.rule_count());
+
+        let _proper_grammar: ProperGrammar = grammar.validate().unwrap();
+    }
+
+    #[test]
+    fn unproductive_grammar_is_not_proper() {
+        let (start, mut grammar): (Nonterminal, Grammar) = Grammar::new();
+        // S -> X
+        // X -> a
+        // X -> Y
+
+        let x = grammar.add_nonterminal();
+        let y = grammar.add_nonterminal();
+        let a = grammar.add_terminal();
+
+        grammar.add_rule(start).nonterminal(x);
+        grammar.add_rule(x).terminal(a);
+        grammar.add_rule(x).nonterminal(y);
+
+        let error: Error = grammar.validate().unwrap_err();
+        assert_eq!(Error::UnproductiveNonterminals(vec![start, y]), error);
     }
 }
