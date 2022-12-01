fa48ed0fc8313843e8250330287562c333b6fd5f
qzvk <reisuo@protonmail.com>
2022-12-01 22:26:12 +0000

    Properly implement lexer.

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -4,13 +4,64 @@
 //! Lexical analysis crate. Provides the `lex` function, which converts a string into a stream of
 //! tokens.
 
+use std::{iter::Peekable, str::CharIndices};
+
 /// A kind of token. The syntactic function of a token.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum TokenKind {}
+pub enum TokenKind {
+    /// The ';'' character
+    Semicolon,
+
+    /// The '=' character
+    Equals,
+
+    /// The '(' character
+    OpenParen,
+
+    /// The ')' character
+    CloseParen,
+
+    /// The '+' character
+    Plus,
+
+    /// The '-' character
+    Minus,
+
+    /// The '*' character
+    Asterisk,
+
+    /// The '/' character
+    Slash,
+
+    /// An identifier (an ASCII letter or underscore, followed by zero or more underscores and
+    /// ASCII alphanumeric characters)
+    Ident,
+
+    /// An integer (one or more digits)
+    Integer,
+
+    /// An unkown token.
+    Unknown,
+}
 
 /// A span of the input string.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct Span {}
+pub struct Span<'a> {
+    line: u32,
+    column: u32,
+    source: &'a str,
+}
+
+impl<'a> Span<'a> {
+    /// Create a new span with the given `line`, `column` and `source`.
+    pub const fn new(line: u32, column: u32, source: &'a str) -> Self {
+        Self {
+            line,
+            column,
+            source,
+        }
+    }
+}
 
 /// An iterator over the tokens of an input string.
 pub fn lex(input: &str) -> impl Iterator<Item = (TokenKind, Span)> + '_ {
@@ -18,19 +69,119 @@ pub fn lex(input: &str) -> impl Iterator<Item = (TokenKind, Span)> + '_ {
 }
 
 struct Tokens<'a> {
-    _input: &'a str,
+    input: &'a str,
+    line: u32,
+    column: u32,
+    chars_indices: Peekable<CharIndices<'a>>,
 }
 
 impl<'a> Tokens<'a> {
     fn new(input: &'a str) -> Self {
-        Self { _input: input }
+        Self {
+            input,
+            line: 0,
+            column: 0,
+            chars_indices: input.char_indices().peekable(),
+        }
+    }
+
+    fn next_ident(&mut self, column: u32, start: usize) -> Option<(TokenKind, Span<'a>)> {
+        match self.chars_indices.peek() {
+            None => Some((
+                TokenKind::Ident,
+                Span::new(self.line, column, &self.input[start..]),
+            )),
+
+            Some(&(_, c)) if c.is_ascii_alphanumeric() || c == '_' => {
+                self.chars_indices.next();
+                self.column += 1;
+                self.next_ident(column, start)
+            }
+
+            Some(&(end, _)) => Some((
+                TokenKind::Ident,
+                Span::new(self.line, column, &self.input[start..end]),
+            )),
+        }
+    }
+
+    fn next_integer(&mut self, column: u32, start: usize) -> Option<(TokenKind, Span<'a>)> {
+        match self.chars_indices.peek() {
+            None => Some((
+                TokenKind::Integer,
+                Span::new(self.line, column, &self.input[start..]),
+            )),
+
+            Some(&(_, c)) if c.is_ascii_digit() => {
+                self.chars_indices.next();
+                self.column += 1;
+                self.next_integer(column, start)
+            }
+
+            Some(&(end, _)) => Some((
+                TokenKind::Integer,
+                Span::new(self.line, column, &self.input[start..end]),
+            )),
+        }
+    }
+
+    fn next_unknown(&mut self, column: u32, start: usize) -> Option<(TokenKind, Span<'a>)> {
+        match self.chars_indices.peek() {
+            None => Some((
+                TokenKind::Unknown,
+                Span::new(self.line, column, &self.input[start..]),
+            )),
+
+            Some(&(end, c)) if is_known(c) => Some((
+                TokenKind::Unknown,
+                Span::new(self.line, column, &self.input[start..end]),
+            )),
+
+            Some(_) => {
+                self.chars_indices.next();
+                self.column += 1;
+                self.next_unknown(column, start)
+            }
+        }
     }
 }
 
 impl<'a> Iterator for Tokens<'a> {
-    type Item = (TokenKind, Span);
+    type Item = (TokenKind, Span<'a>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        let (index, c) = self.chars_indices.next()?;
+        let column = self.column;
+        self.column += 1;
+
+        match c {
+            ';' => Some((TokenKind::Semicolon, Span::new(self.line, column, ";"))),
+            '=' => Some((TokenKind::Equals, Span::new(self.line, column, "="))),
+            '(' => Some((TokenKind::OpenParen, Span::new(self.line, column, "("))),
+            ')' => Some((TokenKind::CloseParen, Span::new(self.line, column, ")"))),
+            '+' => Some((TokenKind::Plus, Span::new(self.line, column, "+"))),
+            '-' => Some((TokenKind::Minus, Span::new(self.line, column, "-"))),
+            '*' => Some((TokenKind::Asterisk, Span::new(self.line, column, "*"))),
+            '/' => Some((TokenKind::Slash, Span::new(self.line, column, "/"))),
+            '\n' => {
+                self.line += 1;
+                self.column = 0;
+                self.next()
+            }
+            c if c.is_ascii_alphabetic() || c == '_' => self.next_ident(column, index),
+            c if c.is_ascii_digit() || c == '_' => self.next_integer(column, index),
+            c if c.is_ascii_whitespace() => self.next(),
+
+            c if !is_known(c) => self.next_unknown(column, index),
+            _ => unreachable!(),
+        }
+    }
+}
+
+const fn is_known(c: char) -> bool {
+    match c {
+        ';' | '=' | '(' | ')' | '+' | '-' | '*' | '/' | '_' => true,
+        c if c.is_ascii_alphanumeric() || c.is_ascii_whitespace() => true,
+        _ => false,
     }
 }
--- lexer/tests/tests.rs
+++ lexer/tests/tests.rs
@@ -1,17 +1,103 @@
+use lexer::{Span, TokenKind};
+
 macro_rules! example {
     ($name:ident : $from:literal => [ $($to:expr),* $(,)? ]) => {
         #[test]
         fn $name() {
-            let input = $from;
-            let expected: &[(::lexer::TokenKind, ::lexer::Span)] = &[
+            const INPUT: &str = $from;
+            const EXPECTED: &[(::lexer::TokenKind, ::lexer::Span)] = &[
                 $(
                     $to,
                 )*
             ];
-            let actual: Vec<_> = ::lexer::lex(input).collect();
-            assert_eq!(expected, actual.as_slice(), "Input was {input}");
+            let actual: ::std::vec::Vec<_> = ::lexer::lex(INPUT).collect();
+            assert_eq!(EXPECTED, actual.as_slice(), "Input was {INPUT:?}");
         }
     };
 }
 
-example! { empty : "" => [] }
+example! {
+    empty: "" => []
+}
+
+example! {
+    single_characters: ";=()+-*/" => [
+        (TokenKind::Semicolon, Span::new(0, 0, ";")),
+        (TokenKind::Equals, Span::new(0, 1, "=")),
+        (TokenKind::OpenParen, Span::new(0, 2, "(")),
+        (TokenKind::CloseParen, Span::new(0, 3, ")")),
+        (TokenKind::Plus, Span::new(0, 4, "+")),
+        (TokenKind::Minus, Span::new(0, 5, "-")),
+        (TokenKind::Asterisk, Span::new(0, 6, "*")),
+        (TokenKind::Slash, Span::new(0, 7, "/")),
+    ]
+}
+
+example! {
+    idents: "a BCD efg_hij _klm NO_P_Q _10x" => [
+        (TokenKind::Ident, Span::new(0, 0, "a")),
+        (TokenKind::Ident, Span::new(0, 2, "BCD")),
+        (TokenKind::Ident, Span::new(0, 6, "efg_hij")),
+        (TokenKind::Ident, Span::new(0, 14, "_klm")),
+        (TokenKind::Ident, Span::new(0, 19, "NO_P_Q")),
+        (TokenKind::Ident, Span::new(0, 26, "_10x")),
+    ]
+}
+
+example! {
+    integers: "0 123456789 99999999999999999999999999" => [
+        (TokenKind::Integer, Span::new(0, 0, "0")),
+        (TokenKind::Integer, Span::new(0, 2, "123456789")),
+        (TokenKind::Integer, Span::new(0, 12, "99999999999999999999999999")),
+    ]
+}
+
+example! {
+    newlines: "first\n second\n  third\n" => [
+        (TokenKind::Ident, Span::new(0, 0, "first")),
+        (TokenKind::Ident, Span::new(1, 1, "second")),
+        (TokenKind::Ident, Span::new(2, 2, "third")),
+    ]
+}
+
+example! {
+    small_example: "square n = n * n;\ntwice f x = f (f x);\nsquare twice (5 - 10)" => [
+        (TokenKind::Ident, Span::new(0, 0, "square")),
+        (TokenKind::Ident, Span::new(0, 7, "n")),
+        (TokenKind::Equals, Span::new(0, 9, "=")),
+        (TokenKind::Ident, Span::new(0, 11, "n")),
+        (TokenKind::Asterisk, Span::new(0, 13, "*")),
+        (TokenKind::Ident, Span::new(0, 15, "n")),
+        (TokenKind::Semicolon, Span::new(0, 16, ";")),
+        (TokenKind::Ident, Span::new(1, 0, "twice")),
+        (TokenKind::Ident, Span::new(1, 6, "f")),
+        (TokenKind::Ident, Span::new(1, 8, "x")),
+        (TokenKind::Equals, Span::new(1, 10, "=")),
+        (TokenKind::Ident, Span::new(1, 12, "f")),
+        (TokenKind::OpenParen, Span::new(1, 14, "(")),
+        (TokenKind::Ident, Span::new(1, 15, "f")),
+        (TokenKind::Ident, Span::new(1, 17, "x")),
+        (TokenKind::CloseParen, Span::new(1, 18, ")")),
+        (TokenKind::Semicolon, Span::new(1, 19, ";")),
+        (TokenKind::Ident, Span::new(2, 0, "square")),
+        (TokenKind::Ident, Span::new(2, 7, "twice")),
+        (TokenKind::OpenParen, Span::new(2, 13, "(")),
+        (TokenKind::Integer, Span::new(2, 14, "5")),
+        (TokenKind::Minus, Span::new(2, 16, "-")),
+        (TokenKind::Integer, Span::new(2, 18, "10")),
+        (TokenKind::CloseParen, Span::new(2, 20, ")")),
+    ]
+}
+
+example! {
+    unknown: "£ % \\@#? hello! 100:00" => [
+        (TokenKind::Unknown, Span::new(0, 0, "£")),
+        (TokenKind::Unknown, Span::new(0, 2, "%")),
+        (TokenKind::Unknown, Span::new(0, 4, "\\@#?")),
+        (TokenKind::Ident, Span::new(0, 9, "hello")),
+        (TokenKind::Unknown, Span::new(0, 14, "!")),
+        (TokenKind::Integer, Span::new(0, 16, "100")),
+        (TokenKind::Unknown, Span::new(0, 19, ":")),
+        (TokenKind::Integer, Span::new(0, 20, "00")),
+    ]
+}
